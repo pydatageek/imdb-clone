be69c8836bd1fe61788499636ab8d177aaedd42d
py data geek <pydatageek@gmail.com>
2020-06-24 07:04:32 +0300

    db uncommented

--- core_project/settings.py
+++ core_project/settings.py
@@ -93,11 +93,10 @@ WSGI_APPLICATION = 'core_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
