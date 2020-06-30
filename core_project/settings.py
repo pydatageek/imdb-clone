61ea9fe32cdb64d7f00913fcdf448550b137d579
py data geek <pydatageek@gmail.com>
2020-06-30 07:39:47 +0300

    settings

--- core_project/settings.py
+++ core_project/settings.py
@@ -96,18 +96,11 @@ WSGI_APPLICATION = 'core_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'imdb51',
-        'USER': 'imdb',
-        'PASSWORD': 'imdb678--',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+
 }
 
 
